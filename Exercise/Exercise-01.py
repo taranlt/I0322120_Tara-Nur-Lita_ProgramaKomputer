5f1f6b886cfb5fc487cc6df5dff5682773628fdc
tara <taralitaa@student.uns.ac.id>
2023-03-13 08:14:29 +0700

    Exercise-01

---
+++ Exercise/Exercise-01.py
@@ -0,0 +1,12 @@
+Nama = "Tara Nur Lita"
+NIM = "I0322120"
+Tempat_tinggal = "Jakarta Selatan."
+Visualisasi = "Jakarta Selatan adalah kota yang dikenal sebagai pusat administrasi Ibukota Jakarta. Selain menjadi pusat administrasi Ibukota Jakarta, Jakarta Selatan merupakan kota yang padat penduduk dan juga menjadi pusat bisnis di Ibukota Jakarta. Tak jarang kemacatan di kota ini sangatlah massive, tapi meskipun demikian sebagian banyaknya peran kota Jakarta Selatan sangat berpengaruh terhadap keberlangsungannya adminitrasi ibukota"
+Harapan = "Saya berharap setelah lulus saya bisa mengaplikasikan apa yang telah saya pelajari ke dalam usaha untuk membangun industri Indonesia yang lebih optimal dan efisien baik dari sisi produksi, pengembangan, sumber daya manusia maupun ekonomi."
+
+print('==============================')
+print('Nama     =', Nama)
+print('NIM      =', NIM)
+print('==============================')
+print('Nama saya adalah', Nama, 'dengan NIM I0322120.', 'Saya berasal dari Kota ', Tempat_tinggal, Visualisasi, 'Sebagai Mahasiswa Teknik Industri', Harapan)
+print('==============================')
